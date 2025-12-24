15d393608960f8d9f961779952d09e2efc7bee6d
eerm-lll <leraermolaeva09@gmail.com>
2025-12-24 23:47:31 +0300

    3 задание

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+// 3 задание
+echo "I like pizza<br>";
+
+// 4 задание
+// однострочный комментарий
+
+/*
+многострочный
+комментарий
+*/
+
+echo "I love PHP<br>";
+
+// 5 задание
+$name = "Alex";
+$age = 20;
+
+echo "Hello {$name}<br>";
+echo "You are {$age} years old<br>";
+
+// 6 задание
+$x = 10;
+$y = 5;
+$sum = $x + $y;
+
+echo "Sum: {$sum}<br>";
+
+?>
